f78c3977df5bb001e6cd9abf3244abc6b3555cef
Shending-Help <yusufcheetos99@gmail.com>
2022-08-21 01:22:05 +0200

    cors -- port static --

--- udagram/udagram-api/src/server.ts
+++ udagram/udagram-api/src/server.ts
@@ -1,5 +1,5 @@
 import * as dotenv from "dotenv";
-import cors from 'cors';
+import cors from "cors";
 import express from "express";
 import { sequelize } from "./sequelize";
 
@@ -18,26 +18,32 @@ import { V0_FEED_MODELS, V0_USER_MODELS } from "./controllers/v0/model.index";
   console.log("Database Connected");
 
   const app = express();
-  const port = process.env.PORT || 8080;
+  const port = process.env.PORT;
 
   app.use(bodyParser.json());
 
-  // app.use(cors());
+  app.use(cors());
   // We set the CORS origin to * so that we don't need to
-  // worry about the complexities of CORS. 
-  app.use(cors({
-    "allowedHeaders": [
-      'Origin', 'X-Requested-With',
-      'Content-Type', 'Accept',
-      'X-Access-Token', 'Authorization', 'Access-Control-Allow-Origin',
-      'Access-Control-Allow-Headers',
-      'Access-Control-Allow-Methods'
-    ],
-    "methods": 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
-    "preflightContinue": true,
-    "origin": '*',
-  }));
-
+  // worry about the complexities of CORS.
+  /*  app.use(
+    cors({
+      allowedHeaders: [
+        "Origin",
+        "X-Requested-With",
+        "Content-Type",
+        "Accept",
+        "X-Access-Token",
+        "Authorization",
+        "Access-Control-Allow-Origin",
+        "Access-Control-Allow-Headers",
+        "Access-Control-Allow-Methods",
+      ],
+      methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
+      preflightContinue: true,
+      origin: "*",
+    })
+  );
+*/
   app.use("/api/v0/", IndexRouter);
 
   // Root URI call
