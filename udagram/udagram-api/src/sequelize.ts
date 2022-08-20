42da9cba36c465599680302dc64e98dcc838bf3e
Shending-Help <yusufcheetos99@gmail.com>
2022-08-20 16:45:15 +0200

    sequelize fix

--- udagram/udagram-api/src/sequelize.ts
+++ udagram/udagram-api/src/sequelize.ts
@@ -1,13 +1,6 @@
 import { Sequelize } from "sequelize-typescript";
 import { config } from "./config/config";
 
-export const sequelize = new Sequelize({
-  username: config.username,
-  password: config.password,
-  database: config.database,
-  host: config.host,
-  port: config.port_db,
-
-  dialect: "postgres",
-  storage: ":memory:",
-});
+export const sequelize = new Sequelize(
+  "postgres://postgres:postgres@database-2.csxehnzadrfg.us-east-1.rds.amazonaws.com:5432/postgres"
+);
